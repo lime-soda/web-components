2859473473ea9f182f53a576b7de645e3e10bb1a
Phil Parsons <p-m-p@users.noreply.github.com>
2025-07-05 16:22:50 +0100

    chore: add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import json from '@eslint/json'
+import css from '@eslint/css'
+
+export default tseslint.config(
+  {
+    ignores: ['**/dist/*', '**/node_modules/*', 'storybook-static'],
+  },
+  js.configs.recommended,
+  json.configs.recommended,
+  tseslint.configs.recommended,
+  css.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    languageOptions: { globals: globals.browser },
+  },
+)
--- vitest.config.js
+++ vitest.config.js
@@ -1,14 +1,6 @@
 import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
 import { defineConfig } from 'vitest/config'
-
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin'
-
-const dirname =
-  typeof __dirname !== 'undefined'
-    ? __dirname
-    : path.dirname(fileURLToPath(import.meta.url))
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
@@ -19,7 +11,9 @@ export default defineConfig({
         plugins: [
           // The plugin will run tests for the stories defined in your Storybook config
           // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-          storybookTest({ configDir: path.join(dirname, '.storybook') }),
+          storybookTest({
+            configDir: path.join(import.meta.dirname, '.storybook'),
+          }),
         ],
         test: {
           name: 'storybook',
