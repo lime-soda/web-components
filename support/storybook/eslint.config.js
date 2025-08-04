e8d35e2e75f58586d8ea33462e3970fd81b1ecb8
Phil Parsons <p-m-p@users.noreply.github.com>
2025-08-04 08:58:29 +0100

    feat(storybook): add eslint storybook plugin

--- support/storybook/eslint.config.js
+++ support/storybook/eslint.config.js
@@ -1,13 +1,19 @@
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
+import storybook from 'eslint-plugin-storybook'
 import baseConfig from '@lime-soda/eslint-config'
 
-export default tseslint.config(...baseConfig, {
-  files: ['**/*.ts'],
-  languageOptions: {
-    globals: {
-      ...globals.browser,
-      ...globals.node,
+export default tseslint.config(
+  baseConfig,
+  {
+    files: ['**/*.stories.ts', '**/*.stories.js'],
+    ignores: ['!.storybook'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
   },
-})
+  storybook.configs['flat/recommended'],
+)
